8cdc54787b30ce6a3d99c284b9214c3c24eb3cb6
Anjali <anjalimagar666@gmail.com>
2023-09-19 21:07:54 -0400

    Initial Commit

---
+++ Control Fun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Control Fun
+//
+//  Created by Anjali Thapa Magar on 19/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
